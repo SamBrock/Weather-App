67e055a6a52812472e3e5f0beefa5410c5e2d336
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-02-15 21:25:51 +0000

    Changed tab space from 4 to 2.

--- src/components/City.js
+++ src/components/City.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function City(props) {
-    return (
-        <h1>{props.city.name}</h1>
-    )
+	return (
+		<h1>{props.city.name}</h1>
+	)
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { getCelcius } from '../utils'
 
 export default function Weather(props) {
-    return (
-        <div>
-            Temperature: {getCelcius(props.weather.main.temp)}<br />
-            {props.weather.weather[0].main}
-        </div>
-    )
+	return (
+		<div>
+			Temperature: {getCelcius(props.weather.main.temp)}<br />
+			{props.weather.weather[0].main}
+		</div>
+	)
 }
